ca294ba40a57619637280527cad018d1c08bdbaf
Daniel Muller <DMuller@DM.cfl.rr.com>
2017-10-09 19:08:15 -0400

    All API's work - need refining

--- keys.js
+++ keys.js
@@ -4,21 +4,22 @@ var twitterKeys = {
   consumer_key: 'MyeARVjQz9p24DXwggOlOGmK6',
   consumer_secret: 'iGYb9MyGNil01rDWhsWjYLygSnDRP3iCt3pORc0G7lxXxReYlH',
   access_token_key: '916738503945400320-SQsQIHWRtryTEGHwoRcl3EexmkaMVED',
-  access_token_secret: 'wZIWRM8LbX90BnCaXuclPzYn17PCnFDlxXl1x1J4XERpp',
+  access_token_secret: 'wZIWRM8LbX90BnCaXuclPzYn17PCnFDlxXl1x1J4XERpp'
 }
 var spotifyKeys = {
     client_id: 'df05645ca1644de0a70245b25e1fcd2a',
     client_secret: '890ee0a6702545fe92f7075f8edbbcf6'
 }
+var omdb = {
+    dakey: '40e9cece'
+}
 
-module.exports = {
-    twitterKeys: twitterKeys,
-    spotifyKeys: spotifyKeys
-};
+module.exports.twitterKeys = twitterKeys;
+module.exports.spotifyKeys = spotifyKeys;
+module.exports.omdb = omdb;
 
-
-//Spotify
-// Client ID
-// df05645ca1644de0a70245b25e1fcd2a
-// Client Secret
-// 890ee0a6702545fe92f7075f8edbbcf6
+// module.exports = {
+//     twitterKeys: twitterKeys,
+//     spotifyKeys: spotifyKeys,
+    // omdb: key
+// }
--- liri.js
+++ liri.js
@@ -1,13 +1,13 @@
 
 
 //Packages
-var fs = require("fs");
-var keys = require("./keys.js");
 
+var fs = require("fs");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 //---------------------------------
+var keys = require("./keys.js");
 
 var command = process.argv[2];
 var search = process.argv[3];
@@ -18,42 +18,73 @@ var search = process.argv[3];
 //Twitter--------------------------
 function twit(){
     var client = new Twitter({
-        consumer_key: 'keys.twitterKeys.consumer_key',
-        consumer_secret: 'keys.twitterKeys.consumer_secret',
-        access_token_key: 'keys.twitterKeys.access_token_key',
-        access_token_secret: 'keys.twitterKeys.access_token_secret'
+
+        //Need replace these keys:
+        consumer_key: keys.twitterKeys.consumer_key,
+        consumer_secret: keys.twitterKeys.consumer_secret,
+        access_token_key: keys.twitterKeys.access_token_key,
+        access_token_secret: keys.twitterKeys.access_token_secret
+        // consumer_key: 'MyeARVjQz9p24DXwggOlOGmK6',
+        // consumer_secret: 'iGYb9MyGNil01rDWhsWjYLygSnDRP3iCt3pORc0G7lxXxReYlH',
+        // access_token_key: '916738503945400320-SQsQIHWRtryTEGHwoRcl3EexmkaMVED',
+        // access_token_secret: 'wZIWRM8LbX90BnCaXuclPzYn17PCnFDlxXl1x1J4XERpp'
       });
       console.log("Test Good: " + keys.twitterKeys.consumer_key);
+      console.log("Test Good: " + keys.twitterKeys.consumer_secret);
+      console.log("Test Good: " + keys.twitterKeys.access_token_key);
+      console.log("Test Good: " + keys.twitterKeys.access_token_secret);
       var params = {screen_name: 'muller_project'};
       client.get('statuses/user_timeline', params, function(error, tweets, response) {
         // if (!error) {
           console.log(tweets);
-          
         // }
       });
 }
 
-//---------------------------------
-//Spotify
 
+//Spotify--------------------------
 function spot(){
     var spotify = new Spotify({
         id: keys.spotifyKeys.client_id,
         secret: keys.spotifyKeys.client_secret
       });
     console.log("spot keys: " + keys.spotifyKeys.client_id)
-      spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+      spotify.search({ type: 'track', query: 'Yesterday' }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
       console.log(JSON.stringify(data, null, 2)); 
       });
 }
+
 function movie(){
+    request('http://www.omdbapi.com/?apikey=' + keys.omdb.dakey + '&t=' + search+ '&plot=full', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body, null, 2); // Print the HTML for the Google homepage.
+      });
 
+    //   Title of the movie.
+    //   * Year the movie came out.
+    //   * IMDB Rating of the movie.
+    //   * Rotten Tomatoes Rating of the movie.
+    //   * Country where the movie was produced.
+    //   * Language of the movie.
+    //   * Plot of the movie.
+    //   * Actors in the movie.
 }
-function doit(){
 
+function doit(){
+    fs.readFile('random.txt', "utf8", (err, data) => {
+        if (err) throw err;
+        console.log(data);
+      });
+
+
+    // 4. `node liri.js do-what-it-says`   
+    // * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.    
+    //   * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+    //   * Feel free to change the text in that document to test out the feature for other commands.
 }
 
 
